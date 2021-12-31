e2092e67bc6ceb51ad8af84150c35dab2018f28a
kim hoyoung <zero@i-on.net>
2021-12-31 09:46:16 +0900

    Initial commit

---
+++ src/test/java/com/example/miniback/MiniBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
