98bae13e945a5a0c6deab19f6174d0bfa6b9dc39
hozero95 <the.hozero@gmail.com>
2021-12-31 17:05:43 +0900

    Spring Security + JWT 적용 완료 및 로그인, 회원가입 API TEST 완료

--- src/main/java/com/example/miniback/config/security/WebSecurityConfig.java
+++ src/main/java/com/example/miniback/config/security/WebSecurityConfig.java
@@ -75,10 +75,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     // CORS configured
+    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://localhost:8000");
+        configuration.addAllowedOrigin("http://localhost:3000");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
